36b4062d9ce4a3afe5b226ac7e0eb24fe6f3ed27
Haein2809 <50579220+Haein2809@users.noreply.github.com>
2024-06-09 12:01:57 +0900

    Update models.py

--- koBERT_tasks/models.py
+++ koBERT_tasks/models.py
@@ -4,6 +4,61 @@ import sentencepiece as spm
 from transformers import BertModel, BertForMaskedLM, BertForSequenceClassification, BertConfig, AdamW
 from kobert_tokenizer import KoBERTTokenizer
 
+class NUGModel(torch.nn.Module):
+    def __init__(self, model_name, num_labels):
+        super(NUGModel, self).__init__()
+        self.bert = BertModel.from_pretrained(model_name)
+        self.drop = torch.nn.Dropout(p=0.3)
+        self.out = torch.nn.Linear(self.bert.config.hidden_size, num_labels)
+
+    def forward(self, input_ids, attention_mask, labels=None):
+        outputs = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        sequence_output = outputs.last_hidden_state  # (batch_size, seq_len, hidden_size)
+        sequence_output = self.drop(sequence_output)
+        logits = self.out(sequence_output)  # (batch_size, seq_len, num_labels)
+
+        loss = None
+        if labels is not None:
+            loss_fct = torch.nn.CrossEntropyLoss()
+            # 마스킹된 위치에 대한 로스만 계산
+            active_loss = attention_mask.view(-1) == 1
+            active_logits = logits.view(-1, logits.size(-1))
+            active_labels = torch.where(
+                active_loss, labels.view(-1), torch.tensor(loss_fct.ignore_index).type_as(labels)
+            )
+            loss = loss_fct(active_logits, active_labels)
+        return loss, logits
+
+class MURModel(torch.nn.Module):
+    def __init__(self, model_name):
+        super(MURModel, self).__init__()
+        self.bert = BertForMaskedLM.from_pretrained(model_name)
+
+    def forward(self, input_ids, attention_mask, labels=None):
+        outputs = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            labels=labels
+        )
+        loss = outputs.loss
+        logits = outputs.logits
+        return loss, logits
+
+class DORNModel(torch.nn.Module):
+    def __init__(self, model_name, num_labels):
+        super(DORNModel, self).__init__()
+        self.config = BertConfig.from_pretrained(model_name, num_labels=num_labels)
+        self.bert = BertForSequenceClassification.from_pretrained(model_name, config=self.config)
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
+        return logits
+
+
 class EmbeddingEnhancedModel(nn.Module):
     def __init__(self, model_name, tf_idf_words, bert_class):
         super(EmbeddingEnhancedModel, self).__init__()
