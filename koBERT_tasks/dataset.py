9eb63cc7890fbf0abfce675963a67501b1c3c173
Haein2809 <50579220+Haein2809@users.noreply.github.com>
2024-06-09 10:35:10 +0900

    Update dataset.py

--- koBERT_tasks/dataset.py
+++ koBERT_tasks/dataset.py
@@ -41,38 +41,20 @@ class NUGDataset(Dataset):
         }
 
 class MURDataset(Dataset):
-    def __init__(self, dataframe, tokenizer, text_col, max_len, masking_prob=0.2):
+    def __init__(self, dataframe, tokenizer, text_col, max_len, mask_prob=0.15):
         self.tokenizer = tokenizer
         self.dataframe = dataframe
         self.text_col = text_col
         self.max_len = max_len
-        self.masking_prob = masking_prob
-
-        # [MASK] 토큰 ID를 설정
-        self.mask_token_id = self.tokenizer.convert_tokens_to_ids('[MASK]')
-
-        # 세션별로 데이터프레임을 그룹화하여 마스킹할 인덱스 선택
-        self.masked_indices = self._select_masked_indices()
-
-    def _select_masked_indices(self):
-        masked_indices = set()
-        grouped = self.dataframe.groupby('id')
-
-        for session_id, group in grouped:
-            total_messages = len(group)
-            mask_count = max(1, round(total_messages * self.masking_prob))
-            mask_indices = random.sample(list(group.index), mask_count)
-            masked_indices.update(mask_indices)
-
-        return masked_indices
+        self.mask_prob = mask_prob
 
     def __len__(self):
         return len(self.dataframe)
 
     def __getitem__(self, idx):
-        row = self.dataframe.iloc[idx]
-        text = row[self.text_col]
-
+        text = self.dataframe.iloc[idx][self.text_col]
+        
+        # 토큰화 및 인코딩
         encoding = self.tokenizer(
             text,
             padding='max_length',
@@ -83,32 +65,20 @@ class MURDataset(Dataset):
 
         input_ids = encoding['input_ids'].squeeze()
         attention_mask = encoding['attention_mask'].squeeze()
-        target_ids = input_ids.clone()
 
-        # 마스킹 작업
-        if idx in self.masked_indices:
-            for i in range(len(input_ids)):
-                if input_ids[i] not in [
-                    self.tokenizer.cls_token_id,
-                    self.tokenizer.sep_token_id,
-                    self.tokenizer.pad_token_id
-                ]:
-                    rand = random.random()
-                    if rand < 0.8:
-                        input_ids[i] = self.mask_token_id
-                    elif rand < 0.9:
-                        input_ids[i] = random.randint(0, self.tokenizer.vocab_size - 1)
-                else:
-                    target_ids[i] = -100
-        else:
-            target_ids[:] = -100
+        # 토큰 마스킹
+        labels = input_ids.clone()
+        probability_matrix = torch.full(labels.shape, self.mask_prob)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -100  # 마스킹되지 않은 토큰은 -100으로 설정
+        input_ids[masked_indices] = self.tokenizer.mask_token_id
 
         return {
             'input_ids': input_ids,
             'attention_mask': attention_mask,
-            'labels': target_ids
+            'labels': labels,
+            'masked_indices': masked_indices
         }
-
 
 class DORNDataset(Dataset):
     def __init__(self, df, session_col, text_col, order_col, tokenizer, max_length, num_labels):
